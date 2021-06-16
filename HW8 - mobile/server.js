f42fa596a0cf37550c408ce53ab5629beecf1a2e
lunah412 <80185530+lunah412@users.noreply.github.com>
2021-06-15 20:49:02 -0700

    hw8

--- HW8 - mobile/server.js
+++ HW8 - mobile/server.js
@@ -1,55 +1,15 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+var express = require('express');
+var path = require('path');
+var app = express();
 
-'use strict';
+app.use(express.static(path.join(__dirname, 'public')));
 
-// [START app]
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// [START enable_parser]
-// This middleware is available in Express v4.16.0 onwards
-app.use(express.json({extended: true}));
-// [END enable_parser]
-
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+app.get('/',function(request,response){
+    response.sendFile(__dirname + '/public/search.html');
 });
 
-// [START add_display_form]
-app.get('/submit', (req, res) => {
-  res.sendFile(path.join(__dirname, '/static/search.html'));
-});
-// [END add_display_form]
 
-// [START add_post_handler]
-app.post('/submit', (req, res) => {
-  console.log({
-    name: req.body.name,
-    message: req.body.message,
-  });
-  res.send('Thanks for your message!');
-});
-// [END add_post_handler]
 
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+var server = app.listen(8888, function () {
+  console.log('Server running at http://127.0.0.1:8888/');
 });
-// [END app]
-
-module.exports = app;
